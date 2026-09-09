7b1cbd915559b65a62d991055fc93174633678e4
kantesh-vaswani <kantesh2008@gmail.com>
2026-09-10 02:47:03 +0500

    Update Question 3.c

--- Question 3.c
+++ Question 3.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(){
-int distance_in_km;
+float distance_in_km;
 printf("Enter distance in km: ");
-scanf("%d", &distance_in_km);
+scanf("%f", &distance_in_km);
 
-printf("The distance in meters is %dm.\n" , 1000*distance_in_km);
-printf("The distance in centimeters is %dcm." , 100000*distance_in_km);
+printf("The distance in meters is %.1f m.\n" , 1000*distance_in_km);
+printf("The distance in centimeters is %.1f cm." , 100000*distance_in_km);
 return 0;
 }
