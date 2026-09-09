97f35712104544f475005103513c69ca3081ec68
kantesh-vaswani <kantesh2008@gmail.com>
2026-09-09 21:32:32 +0500

    Update question 1.c

--- question 1.c
+++ question 1.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main (){
-    printf("My name is kantesh kumar\n");
-    printf("My age is 18 years\n");
-    printf("I am in CS department\n");
-    printf("My student id is 26k-0652\n");
-    printf("my section is 1D\n");
-    printf("my university name is Fast-nuces");
+    printf("My name is kantesh kumar.\n");
+    printf("I am 18 years old.\n");
+    printf("I am in CS department.\n");
+    printf("My student id is 26k-0652.\n");
+    printf("My section is 1-D.\n");
+    printf("My university name is Fast-nuces.");
     return 0;
 }
